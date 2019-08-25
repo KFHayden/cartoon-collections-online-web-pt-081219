88a418a03948800d616e25a9e7067b078f811c04
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-25 20:05:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 
 def long_planeteer_calls(calls_long)
   calls_long.find do |short_words|
-    if short_words.length < 4
+    if short_words.length > 4
       return false
     else
       return true
